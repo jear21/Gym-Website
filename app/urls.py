9786b172f7a3029b3030cb2bc30473e262cd5612
jear21 <jhonnrheyatilano@gmail.com>
2024-11-06 05:41:50 +0800

    commit

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, BlogListView, BlogDetailView
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('blog/',BlogListView.as_view(), name='blog'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = 'app/home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'app/about.html'
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name ='posts'
+    template_name ='app/blog_list.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name ='app/blog_detail.html'
+    
 
 
